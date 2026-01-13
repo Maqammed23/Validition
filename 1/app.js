10ecd588351ba86c56baab00c6b7d194251d3c6f
Maqammed23 <mehemmed23062012@gmail.com>
2026-01-13 21:11:14 +0400

    kjhvc

--- 1/app.js
+++ 1/app.js
@@ -1,9 +1,41 @@
 $(document).ready(function () {
-    $("#toggle-h2").toggle(2000, function () {
-        if ($("toggle-h2").text()=="Hide") {
-            $("#toggle-h2").text("Show")
-        } else {
-            $("#toggle-h2").text("Hide")
-        }
-    })
-})
+    $("#buttonbir").click(function () {
+        $("#toggle-h2").toggle(2000, function () {
+            if ($("#buttonbir").text() == "Hide") {
+                $("#buttonbir").text("Show");
+            } else {
+                $("#buttonbir").text("Hide");
+            }
+        });
+    });
+    
+    $("#buttoniki").click(function () {
+        $("#fader-h2").fadeToggle(2000, function () {
+            if ($("#buttoniki").text() == "FADE OUT") {
+                $("#buttoniki").text("FADE IN");
+            } else {
+                $("#buttoniki").text("FADE OUT");
+            }
+        });
+    });
+$("#buttonuc").click(function () {
+        $("#fadeto-h2").fadeTo(2000, 0.25, function () {
+            if ($("#buttonuc").text() == "FADE TO 25%") {
+                $("#buttonuc").text("FADE İN");
+            } else {
+                $("#buttonuc").text("FADE TO 25%");
+            }
+        });
+    });
+
+$("#buttondord").click(function () {
+        $("#slide-h2").slideToggle(2000, function () {
+            if ($("#buttondord").text() == "SLIDE DOWN") {
+                $("#buttondord").text("SLIDE UP");
+            } else {
+                $("#buttondord").text("SLIDE DOWN");
+            }
+        });
+    });
+
+});
