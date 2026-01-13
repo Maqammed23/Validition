ed76a8b98e95f691292c6277f3eb17329f183852
Maqammed23 <mehemmed23062012@gmail.com>
2026-01-13 20:16:55 +0400

    kjhgf

--- 1/app.js
+++ 1/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("#toggle-h2").toggle(2000, function () {
-        if ($("toggle").text()=="Hide") {
-            $("#toggle").text("Show")
+        if ($("toggle-h2").text()=="Hide") {
+            $("#toggle-h2").text("Show")
         } else {
             $("#toggle-h2").text("Hide")
         }
