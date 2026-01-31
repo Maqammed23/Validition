739cc0affb02a3ff707270f0f3fe527e118368d4
Maqammed23 <mehemmed23062012@gmail.com>
2026-01-31 12:37:21 +0400

    m

--- 3/app.js
+++ 3/app.js
@@ -50,11 +50,59 @@
 // })
 
 
-let array = [3,8,5,6,12]
-if ( arr1 = array%2 == 0) {
-    console.log(arr1)
-} else {
-    console.log(array)
-}
+// let array = [3,8,5,6,12]
+// if ( arr1 = array%2 == 0) {
+//     console.log(arr1)
+// } else {
+//     console.log(array)
+// }
 
+// $.ajax({
+//     url:  "https://jsonplaceholder.typicode.com/posts/1",
+//     type: "PUT",
+//     headers: {
+//         'Content-type': 'application/json; charset=UTF-8'
+//     },
+//     data: JSON.stringify({
+//         tittle: "SALAM",
+//         body: "SALAM1"
+//     }),
+//     succes: function (data) {
+//         console.log(data)
+//     },
 
+//     error:function (error) {
+//          console.log(error)
+//     }
+// })
+// $.ajax({
+//     url:  "https://jsonplaceholder.typicode.com/posts/1",
+//     type: "PATCH",
+//     headers: {
+//         'Content-type': 'application/json; charset=UTF-8'
+//     },
+//     data: JSON.stringify({
+//         tittle: "samkvsgfad",
+        
+//     }),
+//     succes: function (data) {
+//         alert(data)
+//     },
+
+//     error:function (error) {
+//          alert(error)
+//     }
+// })
+
+// $.ajax({
+//     url:  "https://jsonplaceholder.typicode.com/posts/1",
+//     type: "DELETE",
+   
+//     succes: function (data) {
+//       console.log("Post silindi")
+//     },
+
+//     error:function (error) {
+//          console.log("Post silinmedi")
+//     }
+// })
